de17593da3e58fb6e9824731a58aec88f22836b9
SelvaPraveen309 <selvasl333@gmail.com>
2022-04-27 10:11:13 +0530

    added Spring

---
+++ src/main/java/com/app/Hotel/FoodiesGoAPI/FoodiesGoApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.Hotel.FoodiesGoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodiesGoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodiesGoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/Hotel/FoodiesGoAPI/FoodiesGoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.Hotel.FoodiesGoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodiesGoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
